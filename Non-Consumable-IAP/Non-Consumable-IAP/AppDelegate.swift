8bef078a2ad8d205e1fb8fa1638cb7e48e0382d4
Satnam <29satnam@gmail.com>
2020-08-25 10:46:21 +0530

    init xcode project

---
+++ Non-Consumable-IAP/Non-Consumable-IAP/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Non-Consumable-IAP
+//
+//  Created by Biznatch on 25/08/20.
+//  Copyright © 2020 Silver Seahog. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Non-Consumable-IAP/Non-Consumable-IAP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Non-Consumable-IAP
+//
+//  Created by Biznatch on 25/08/20.
+//  Copyright © 2020 Silver Seahog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
