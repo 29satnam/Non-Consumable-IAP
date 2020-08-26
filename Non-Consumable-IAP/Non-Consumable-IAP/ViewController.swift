aa27148cc4c7e1923a5170924b7dc8e496346ff1
Satnam <29satnam@gmail.com>
2020-08-26 14:59:52 +0530

    added comments

--- Non-Consumable-IAP/Non-Consumable-IAP/ViewController.swift
+++ Non-Consumable-IAP/Non-Consumable-IAP/ViewController.swift
@@ -10,24 +10,35 @@ import UIKit
 import StoreKit
 
 class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
-    
+    // Buttons
     @IBOutlet weak var unlockProButton: UIButton!
     @IBOutlet weak var restorePurchaseButton: UIButton!
+    
+    // Indicator
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    // Product Request Instance
     var productsRequest = SKProductsRequest()
+    
+    // Fetched products array
     var validProducts = [SKProduct]()
+    
+    // Your product's index
     var productIndex = 0
     
     
     // viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Show indicator initially
         activityIndicator.startAnimating()
         activityIndicator.isHidden = false
         
+        // Hide unlock button initially
         unlockProButton.isHidden = true
 
+        // Fetch products on main thread
         DispatchQueue.main.async {
             self.fetchAvailableProducts()
         }
@@ -36,10 +47,12 @@ class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTran
     
     
     func fetchAvailableProducts()  {
+        // Set your product's identifier
         let productIdentifiers = NSSet(objects:
             "com.iap.unlockproversion"  // 1
         )
         productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers as! Set<String>)
+        // Set deleagte and start
         productsRequest.delegate = self
         productsRequest.start()
     }
@@ -48,11 +61,14 @@ class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTran
     
     func productsRequest (_ request:SKProductsRequest, didReceive response:SKProductsResponse) {
         if (response.products.count > 0) {
+            // Get retrieved products
             validProducts = response.products
             
-            // 1st IAP Product
+            // Here's your product at index 0
             let prodUnlockPro = response.products[0] as SKProduct
             print("1st product: " + prodUnlockPro.localizedDescription)
+            
+            // Now that you have product info, show unlock button and stop indicator
             DispatchQueue.main.async {
                 self.unlockProButton.isHidden = false
                 
@@ -62,15 +78,15 @@ class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTran
         }
     }
     
-    
+    // Tells the observer that a user initiated an in-app purchase from the App Store.
     func paymentQueue(_ queue: SKPaymentQueue, shouldAddStorePayment payment: SKPayment, for product: SKProduct) -> Bool {
         return true
     }
     
-    
+    // Tells if a product can be purchased
     func canMakePurchases() -> Bool {  return SKPaymentQueue.canMakePayments()  }
     
-    
+    // Initiate purchase process
     func purchaseMyProduct(_ product: SKProduct) {
         if self.canMakePurchases() {
             let payment = SKPayment(product: product)
@@ -84,7 +100,7 @@ class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTran
         }
     }
     
-    
+    // Update the user with purchase result
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
         for transaction:AnyObject in transactions {
             if let trans:SKPaymentTransaction = transaction as? SKPaymentTransaction {
@@ -128,23 +144,22 @@ class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTran
                 }
             }
         }
-    }
+    }    
     
-    
-    
+    // Initiate restore
     func restorePurchase() {
         SKPaymentQueue.default().add(self as SKPaymentTransactionObserver)
         SKPaymentQueue.default().restoreCompletedTransactions()
     }
     
+    // Tells if restore succeeded 
     func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
         print("The Payment was successfull!")
     }
     
     
     
-    // Buttons -------------------------------------
-    
+    // Buy IAP button -------------------------------------
     @IBAction func unlockProButt(_ sender: UIButton) {
         productIndex = 0
         purchaseMyProduct(validProducts[productIndex])
@@ -158,7 +173,7 @@ class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTran
         
     }
     
-    
+    // Restore IAP button
     @IBAction func restorePurchaseButt(_ sender: UIButton) {
         
         restorePurchase()
@@ -171,7 +186,7 @@ class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTran
         }
     }
     
-    
+    // Custom alert control function
     func makeAlert(title: String, message: String, buttonTitle: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
